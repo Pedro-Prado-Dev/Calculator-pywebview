d9509051ea2e035e550e15c712f9b7e1d9d6b770
Pedro_Prado <pedro.paiva@inatel.br>
2024-01-04 10:35:18 -0300

    adding localStorage for dark mode

--- calculator-project/app/assets/script.js
+++ calculator-project/app/assets/script.js
@@ -1,4 +1,13 @@
 function toggleDarkMode() {
     const body = document.body;
-    body.classList.toggle("dark-mode");
+    const isDarkMode = body.classList.toggle("dark-mode");
+    localStorage.setItem('darkMode', isDarkMode);
 }
+
+window.onload = function () {
+    const darkMode = localStorage.getItem('darkMode');
+    const body = document.body;
+    if (darkMode === 'true') {
+        body.classList.add('dark-mode');
+    }
+};
